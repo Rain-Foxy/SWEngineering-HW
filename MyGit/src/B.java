0afee5b232da2c992663026f566b3bd4122ed163
buddy <buddy@DESKTOP-1UA6CHI>
2022-06-21 19:06:27 +0900

    Project first commit

---
+++ MyGit/src/B.java
@@ -0,0 +1,7 @@
+
+
+public class B {
+	public void sayHello() {
+		System.out.println("Create a Hello class");
+	}
+}
---
+++ MyGit/src/C.java
@@ -0,0 +1,7 @@
+
+
+public class C {
+	public void sayBye() {
+		System.out.println("Create a Bye class");
+	}
+}
